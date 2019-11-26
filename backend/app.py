3483b6c3d14f340f8242158f10677d62aae52a54
n0bis <madsfalken@Madss-MacBook-Pro.local>
2019-11-26 18:57:19 +0100

    missing connection to sql and allow for post methods

--- backend/app.py
+++ backend/app.py
@@ -1,29 +1,49 @@
 #!/usr/bin/python3
 
 from flask import Flask, request, jsonify
+from flaskext.mysql import MySQL
 import os
 
 application = Flask(__name__)
 
-#app.config["MYSQL_URI"] = 'mongodb://' + os.environ.get('MONGODB_USERNAME') + ':' + os.environ['MONGODB_PASSWORD'] + '@' + os.environ['MONGODB_HOSTNAME'] + ':27017/' + os.environ['MONGODB_DATABASE']
+application.config['MYSQL_DATABASE_USER'] = os.environ['MYSQL_DATABASE_USER']
+application.config['MYSQL_DATABASE_PASSWORD'] = os.environ['MYSQL_DATABASE_PASSWORD']
+application.config['MYSQL_DATABASE_DB'] = os.environ['MYSQL_DATABASE_DB']
+application.config['MYSQL_DATABASE_HOST'] = os.environ['MYSQL_DATABASE_HOST']
+application.config['MYSQL_DATABASE_PORT'] = int(os.environ['MYSQL_DATABASE_PORT'])
+
+mysql = MySQL()
+mysql.init_app(application)
 
 @application.route('/persons/', methods=['GET'])
 def getPersons():
-  return jsonify(
-    Firstname= 'Hello',
-    Lastname= 'World!'
-  )
+  try:
+    conn = mysql.connect()
+    cur = mysql.get_db().cursor()
+    cur.execute('SELECT * FROM persons')
+    rows = cur.fetchall()
+    response = jsonify(rows)
+    response.status_code = 200
+    return response
+  except Exception as e:
+    print(e)
+    return str(e), 500
 
 @application.route('/person/', methods=['POST'])
 def createPerson():
-  data = request.get_json(force=True)
-  person = {
-    'PersonID': 0,
-    'Firstname': data['Firstname'],
-    'Lastname': data['Lastname']
-  }
+  try:
+    conn = mysql.connect()
+    cur = mysql.get_db().cursor()
+    data = request.get_json(force=True)
 
-  return jsonify(person), 201
+    cur.execute('INSERT INTO persons (Firstname, Lastname) VALUES (%(firstname)s, %(lastname)s)', { 'firstname': data['Firstname'], 'lastname': data['Lastname']})
+    conn.commit()
+    response = ''
+    response.status_code = 201
+    return response
+  except Exception as e:
+    print(e)
+    return str(e), 500
 
 if __name__ == '__main__':
   ENVIRONMENT_DEBUG = os.environ.get("APP_DEBUG", True)
